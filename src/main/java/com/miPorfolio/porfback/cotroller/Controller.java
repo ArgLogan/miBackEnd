1ca1fda8ca7cf22be4ce4a19ecca7dcf9ad0af6d
JJRoma <jj_roma@yahoo.ccom.ar>
2022-08-10 20:27:55 -0300

    retomaando

--- src/main/java/com/miPorfolio/porfback/cotroller/Controller.java
+++ src/main/java/com/miPorfolio/porfback/cotroller/Controller.java
@@ -18,6 +18,7 @@ import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+//@CrossOrigin(origins={"http://localhost:4200"})
 public class Controller {
     
     @Autowired IPersonaService persoServ;
@@ -192,6 +194,7 @@ public class Controller {
     @ResponseBody    
     public boolean verUser(@RequestBody Users user){
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        
         List<Users> usuarios =  userServ.verUsers();
         
         for( Users x:usuarios){
